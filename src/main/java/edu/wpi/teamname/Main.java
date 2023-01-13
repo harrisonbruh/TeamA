388b45613edd3d72f65050b1c47bbf0a9ef905b2
unknown <amborn02@gmail.com>
2023-01-13 16:18:35 -0500

    Added print statement for Andrei

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -6,6 +6,7 @@ public class Main {
     System.out.println("Audrey");
     System.out.println("Harrison");
     System.out.println("Sarah");
+    System.out.println("Andrei");
     App.launch(App.class, args);
     System.out.println("Isabella Lucas");
   }
