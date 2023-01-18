41747dbfc634d1c287b00bb5db2adc38f4ff0afc
Jay <jcmezzo@wpi.edu>
2023-01-18 16:17:51 -0500

    Print Name "Jay"

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -6,6 +6,7 @@ public class Main {
     System.out.println("Audrey");
     System.out.println("Harrison");
     System.out.println("Sarah");
+    System.out.println("Jay");
     App.launch(App.class, args);
   }
 }
